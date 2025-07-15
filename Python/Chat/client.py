470f664ffa219aaec0803c44870d294c19bd1b48
AustinWithoutPowers <austinrudkin@live.com>
2025-07-15 14:26:43 +1200

    Moved chat client and server out of SocketWithoutPowers and into here instead. Will be finding a proper way to package SWP and then import it here.

---
+++ Python/Chat/client.py
@@ -0,0 +1,14 @@
+import python.SocketWithoutPowers as SWP
+
+DEBUGGING = False
+
+def main():
+    message = ''
+    for i in range(1024):
+        message += str(i)
+
+    client_chat = SWP.ChatClient(debug=DEBUGGING)
+    client_chat.chat_loop(end_command='qwer')
+    
+    
+main()
